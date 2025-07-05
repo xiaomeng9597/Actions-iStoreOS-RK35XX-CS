510ddbbfa0f2cc245c27ee48b5fbcfeca12fefa8
519561910@qq.com <519561910@qq.com>
2025-07-06 00:41:03 +0800

    update diy-part3-6.x.sh

--- diy-part3-6.x.sh
+++ diy-part3-6.x.sh
@@ -18,8 +18,8 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 
 
 # 集成CPU性能跑分脚本
-echo "CONFIG_PACKAGE_coremark=y" >> .config
-cp -f $GITHUB_WORKSPACE/configfiles/coremark/coremark.sh package/base-files/files/bin/coremark.sh
+cp -a $GITHUB_WORKSPACE/configfiles/coremark/* package/base-files/files/bin/
+chmod 755 package/base-files/files/bin/coremark
 chmod 755 package/base-files/files/bin/coremark.sh
 
 
